a269ee1f080e2f033d5b23794f1835425164b421
Luis Henrique Franco <luishbfranco@hotmail.com>
2020-03-29 14:47:25 -0300

    Template Strings

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -3,16 +3,18 @@ class DateHelper{
     constructor(){
         throw new Error('DateHelper não pode ser instanciada!');
     }
-    
+
     static textoParaData(texto){
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('O texto deve estar no formato yyyy-mm-dd');
+
         return new Date(...texto.split('-')
         .map((item, indice) => indice == 1 ? item - 1 : item ));
     }
 
     static dataParaTexto(data){
-        return data.getDate()
-        + '/' + (data.getMonth() + 1)
-        + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
     
 }
