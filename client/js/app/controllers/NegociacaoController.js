069a833be7baa71b29d3a26a34a69c263334edbb
Luis Henrique Franco <luishbfranco@hotmail.com>
2020-03-29 13:49:40 -0300

    melhorando a arrow function da instancia de data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,9 +12,7 @@ class NegociacaoController{
         event.preventDefault();
 
         let data = new Date(...this.inputData.value.split('-')
-        .map((item, indice) => {
-            return indice == 1 ? item - 1 : item
-        }));
+        .map((item, indice) => indice == 1 ? item - 1 : item ));
 
         let negociacao = new Negociacao(
             data,
